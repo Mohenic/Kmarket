dd3999c5552330c360886548769f674c92cadccf
Mohenic <syw077@gmail.com>
2023-09-13 12:26:17 +0900

    CS article table 생성 및 JSP 수정

---
+++ Kmarket/src/main/java/kr/co/kmarket/db/DBHelper.java
@@ -0,0 +1,56 @@
+package kr.co.kmarket.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBHelper {
+	
+	protected Connection conn = null;
+	protected PreparedStatement psmt = null;
+	protected PreparedStatement psmt1 = null;
+	protected Statement stmt = null;
+	protected ResultSet rs = null;
+	
+	public Connection getConnection() {
+		
+		try {
+			Context ctx = (Context) new InitialContext().lookup("java:comp/env");
+			DataSource ds = (DataSource) ctx.lookup("jdbc/Kmarket");
+			conn = ds.getConnection();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	public void close() throws SQLException {
+		if(rs != null) {
+			rs.close();
+		}
+		
+		if(stmt != null) {
+			stmt.close();
+		}
+		
+		if(psmt != null) {
+			psmt.close();
+		}
+		
+		if(psmt1 != null) {
+			psmt1.close();
+		}
+		
+		if(conn != null) {
+			conn.close();
+		}
+	}
+
+}
